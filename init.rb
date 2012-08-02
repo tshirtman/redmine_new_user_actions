094949c898834917538932facbcdecf05dc0370e
Gabriel Pettier <gabriel.pettier@alterway.fr>
2012-08-02 18:11:40 +0200

    add init.rb hooks and config

---
+++ init.rb
@@ -0,0 +1,15 @@
+require 'user_hooks'
+
+Redmine::Plugin.registor :redmine_new_user_actions do
+  name 'Redmine New User Actions'
+  author 'gabriel.pettier@alterway.fr'
+  description 'Bind actions to new users creation'
+  version '0.0.1'
+  url ''
+  author_url 'alterway.fr'
+
+  settings(:default => {
+    'add_to_projects' => ""
+  },
+    :partial => 'settings/new_user_actions')
+end
---
+++ lib/user_hooks.rb
@@ -0,0 +1,20 @@
+module NewUserActionsPlugin
+  class Hooks < Redmine::Hook:ViewListener
+
+    def controller_users_success_creation(context={})
+      if Setting.plugin_redmine_new_user_actions['add_to_projects']
+        Setting.plugin_redmine_new_user_actions['add_to_projects'].split("\n").each do |line|
+          project, role = line.split(':')
+          project = Project.find_by_identifier(project.strip)
+          role = Role.find_by_name role.strip
+
+          member = Member.new
+          member.user = context[:user]
+          member.project = project
+          member.role = role
+          member.save
+        end
+      end
+    end
+  end
+end
